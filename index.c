0ad3bc16bdb39fdce87a173c4721aa81b6b9f0c7
Terance Edmonds <teranceedmonds20@gmail.com>
2023-02-13 11:36:25 +0530

    float data type usage

--- index.c
+++ index.c
@@ -4,16 +4,17 @@
 int main()
 {
     // allocate memory for an integer
-    int *i = (int *)MyMalloc(4);
-    int *j = (int *)MyMalloc(4);
+    int *i = (int *)MyMalloc(sizeof(int));
+    float *j = (float *)MyMalloc(sizeof(float));
     *i = 7;
-    *j = 10;
+    *j = 10.5;
 
     printf("%d\n", *i);
-    printf("%d", *j);
+    printf("%f\n", *j);
 
     // free the allocated memory
     MyFree(i);
+    MyFree(j);
 
     return 0;
 }
